f388f8ada9776b47471d760472651767f7c5ab0c
lundellm <maija.lundell@tuni.fi>
2023-06-14 09:57:34 +0300

    Differientated component Course

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,85 @@
+const Part = props => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        {props.name} {props.exercises}
+      </p>
+    </div>
+  )
+}
+
+const Header = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>
+        {props.course}
+      </h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        {props.text} {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+      </p>
+    </div>
+  )
+}
+
+const Course = (props) => {
+  console.log(props)
+  return(
+    <div>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
