43f43e76d180d8c2ea3faee0595e9af5d1eb9d10
lundellm <maija.lundell@tuni.fi>
2023-08-17 15:37:56 +0300

    The weather forecast of the capital is shown

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -8,54 +8,57 @@ const App = () => {
   const [search, setSearch] = useState('')
   const [countries, setCountries] = useState([])
 
+  const api_key = process.env.REACT_APP_WEATHER_API_KEY;
 
-const getCountries = () => {
-  if (search === '') {
-    setCountries([])
-    return
-  }
-  console.log('fetching country informations...')
-  axios
-    .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
-    .then(response => {
-      const resultCountries = response.data.filter(country =>
-        country.name.common.toLowerCase().includes(search.toLowerCase()))
-      setCountries(resultCountries)
-    })
-}
+  const getCountries = () => {
+    if (search === '') {
+      setCountries([]);
+      return;
+    }
+    console.log('fetching country informations...');
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+      .then(response => {
+        const resultCountries = response.data.filter(country =>
+          country.name.common.toLowerCase().includes(search.toLowerCase())
+        );
+        setCountries(resultCountries);
+      });
+  };
+
   useEffect(() => {
-    getCountries()
-  }, [search])
-    
-  const handleChange = (event) => {
-    setSearch(event.target.value)
-  }
+    getCountries();
+  }, [search]);
 
-  const showCountry = (country) => {
-    setCountries([country])
-  }
+  const handleChange = event => {
+    setSearch(event.target.value);
+  };
 
-  return (
-    <div>
+  const showCountry = country => {
+    setCountries([country]);
+  };
+
+    return (
       <div>
-      <Filter value={search} onChange={handleChange} />
+        <div>
+        <Filter value={search} onChange={handleChange} />
+        </div>
+        <div>
+        {countries.length > 10 ? (
+          <p>Too many matches, specify another filter</p>
+        ) : countries.length > 1 ? (
+          <ul>
+            {countries.map(country => (
+              <li key={country.name.common} >
+                {country.name.common} <button onClick={() => showCountry(country)}>show</button>
+              </li>
+            ))}
+          </ul>
+        ) : countries.length === 1 && <Country country={countries[0]} weatherApiKey={api_key} />}
+        </div>
       </div>
-      <div>
-      {countries.length > 10 ? (
-        <p>Too many matches, specify another filter</p>
-      ) : countries.length > 1 ? (
-        <ul>
-          {countries.map(country => (
-            <li key={country.name.common} >
-              {country.name.common} <button onClick={() => showCountry(country)}>show</button>
-            </li>
-          ))}
-        </ul>
-      ) : countries.length === 1 && <Country country={countries[0]} />}
-      </div>
-    </div>
-    
-  )
-}
+      
+    )
+  }
 
 export default App
--- osa2/maiden_tiedot/src/components/Country.js
+++ osa2/maiden_tiedot/src/components/Country.js
@@ -1,20 +1,44 @@
-const Country = ({country}) => {
-    return (
-        <div>
-          <h2>{country.name.common}</h2>
-          <p>capital {country.capital}<br></br>area {country.area}</p>
-          <h4>languages:</h4>
-          <ul>
-            {Object.values(country.languages).map(language => (
-              <li key={language}>{language}</li>
-            ))}
-          </ul>
-          <img
-            src={country.flags.png}
-            alt={`${country.name.common} flag`}
-            width="200"
-            />
-        </div>
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Country = ({ country, weatherApiKey }) => {
+    const [weatherData, setWeatherData] = useState(null);
+
+    useEffect(() => {
+        axios
+        .get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital[0]}&appid=${weatherApiKey}`)
+        .then(response => {
+            setWeatherData(response.data);
+        });
+    }, [country.capital[0]]);
+
+return (
+    <div>
+        <h2>{country.name.common}</h2>
+        <p>capital {country.capital}<br></br>area {country.area}</p>
+        <h4>languages:</h4>
+        <ul>
+        {Object.values(country.languages).map(language => (
+            <li key={language}>{language}</li>
+        ))}
+        </ul>
+        <img
+        src={country.flags.png}
+        alt={`${country.name.common} flag`}
+        width="200"
+        />
+        
+        {weatherData && (
+            <div>
+            <h3>Weather in {country.capital[0]}</h3>
+            <p>temperature {weatherData.main.temp} Celsius</p>
+            <img src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`} alt ={weatherData.weather[0].desctiption} />
+            <p>wind {weatherData.main.wind} m/s</p>
+            </div>
+        )
+        }
+    </div>
     )
 }
 
